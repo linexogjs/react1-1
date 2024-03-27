cc6515915f30af063cda13d0b466cc7729d09e3c
linexogjs <linexogjs1234@gmail.com>
2024-03-27 11:18:47 +0900

    test-app

---
+++ test-app/src/Hello.js
@@ -0,0 +1,8 @@
+class Hello extends React.Component {
+    render() {
+        return <div>Hello {this.props.toWhat}</div>
+    }
+}
+//const root = ReactDom.createRoot(document.getElementById('root'))
+//root.render(<Hello toWhat="World"/>)
+export default Hello;
